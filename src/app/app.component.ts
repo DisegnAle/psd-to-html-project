d606cd562884c4acb4fe3ace85b7e314e882c1b5
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-27 17:09:35 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'psd-to-html2-project';
+}

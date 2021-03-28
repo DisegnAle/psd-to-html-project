b69767b71fa88938b460913c3e4c398f14e191b0
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-28 10:10:39 +0200

    ref cart name

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app__cart',
+  selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })

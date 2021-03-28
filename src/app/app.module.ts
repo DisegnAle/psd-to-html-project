bc00974f7cfd59ba3b4c9015e71bf823cf368ba2
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-28 10:23:09 +0200

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { IntroductionComponent } from './components/introduction/introduction.component';
+import { IntroductionComponent } from './components/sections/introduction/introduction.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CharacteristicsComponent } from './components/characteristics/characteristics.component';
-import { SpecificationsComponent } from './components/specifications/specifications.component';
-import { CartComponent } from './components/cart/cart.component';
+import { CharacteristicsComponent } from './components/sections/characteristics/characteristics.component';
+import { SpecificationsComponent } from './components/sections/specifications/specifications.component';
+import { CartComponent } from './components/sections/cart/cart.component';
 import { ChangeColorToHeaderDirective } from './directives/change-color-to-header.directive';
 import { SidePanelComponent } from './components/side-panel/side-panel.component';
 import { SwiperModule } from 'swiper/angular';
 import { MobileSliderComponent } from './components/mobile-slider/mobile-slider.component';
+import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { MobileSliderComponent } from './components/mobile-slider/mobile-slider.
     CartComponent,
     ChangeColorToHeaderDirective,
     SidePanelComponent,
-    MobileSliderComponent
+    MobileSliderComponent,
+    MenuMobileComponent
   ],
   imports: [
     BrowserModule,

47e89091a6f65ca078941ec1622353ebc6283e68
Alessio Scaiola <alessio.scaiola@exmachina.ch>
2021-03-28 09:28:23 +0200

    separate styles

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cart',
+  selector: 'app__cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
